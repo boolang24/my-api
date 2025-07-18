660794ed2311eb1f4d207339155e619cbae64321
boolang24 <dafadaxx023@gmail.com>
2025-07-18 19:27:00 +0700

    Update ip.js

--- api/ip.js
+++ api/ip.js
@@ -1,22 +1,14 @@
 export default async function handler(req, res) {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  const ua = req.headers['user-agent'] || '';
-  
-  // Contoh geo dummy
-  const country = 'Indonesia';
-  const city = 'Jakarta';
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
-  // Kirim data ke Google Sheet lewat Apps Script
-  await fetch("https://script.google.com/macros/s/AKfycbyhdZb-LWztRf6a6J1KoD2GwuHKk0jZMVWeie4eZgOCMfM5VyBCnE6faT9fRb4I__co/exec", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      ip: ip,
-      ua: ua,
-      country: country,
-      city: city
-    })
-  });
+  try {
+    const sheetURL = "https://script.google.com/macros/s/AKfycbysCsXfJZ-SFk4gPJ-LrZK6f6Hl64VJc0NeZdDQg5yPdup0-g7Y9dyUr8NYX1LqSRgU/exec";
+    const send = await fetch(sheetURL + `?ip=${ip}`);
+    const result = await send.text();
 
-  res.status(200).json({ ip, ua, country, city });
+    res.status(200).json({ ip });
+  } catch (error) {
+    console.error("Gagal kirim ke Google Sheet:", error);
+    res.status(500).json({ error: "Gagal kirim ke Sheet" });
+  }
 }
