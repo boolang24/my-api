dee522c2ae6327721434ad01b55dff87a8a63d68
boolang24 <dafadaxx023@gmail.com>
2025-07-18 19:32:01 +0700

    Update ip.js

--- api/ip.js
+++ api/ip.js
@@ -1,14 +1,15 @@
 export default async function handler(req, res) {
   const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const sheetURL = "https://script.google.com/macros/s/AKfycbysCsXfJZ-SFk4gPJ-LrZK6f6Hl64VJc0NeZdDQg5yPdup0-g7Y9dyUr8NYX1LqSRgU/exec";
 
   try {
-    const sheetURL = "https://script.google.com/macros/s/AKfycbysCsXfJZ-SFk4gPJ-LrZK6f6Hl64VJc0NeZdDQg5yPdup0-g7Y9dyUr8NYX1LqSRgU/exec";
-    const send = await fetch(sheetURL + `?ip=${ip}`);
-    const result = await send.text();
+    const response = await fetch(sheetURL + `?ip=${ip}`);
+    const text = await response.text();
+    console.log("Hasil fetch ke Google Sheet:", text);
 
-    res.status(200).json({ ip });
+    res.status(200).json({ ip, googleResponse: text });
   } catch (error) {
-    console.error("Gagal kirim ke Google Sheet:", error);
-    res.status(500).json({ error: "Gagal kirim ke Sheet" });
+    console.error("Gagal kirim ke Sheet:", error);
+    res.status(500).json({ error: "Gagal kirim ke Google Sheet" });
   }
 }
