4c7529becfaa5d6863901c55fc62070389b58e55
boolang24 <dafadaxx023@gmail.com>
2025-07-18 19:10:53 +0700

    Update ip.js

--- api/ip.js
+++ api/ip.js
@@ -1,5 +1,22 @@
-export default function handler(req, res) {
-  const forwarded = req.headers["x-forwarded-for"];
-  const ip = forwarded ? forwarded.split(",")[0] : req.socket.remoteAddress;
-  res.status(200).json({ ip });
+export default async function handler(req, res) {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ua = req.headers['user-agent'] || '';
+  
+  // Contoh geo dummy
+  const country = 'Indonesia';
+  const city = 'Jakarta';
+
+  // Kirim data ke Google Sheet lewat Apps Script
+  await fetch("https://script.google.com/macros/s/AKfycbyhdZb-LWztRf6a6J1KoD2GwuHKk0jZMVWeie4eZgOCMfM5VyBCnE6faT9fRb4I__co/exec", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      ip: ip,
+      ua: ua,
+      country: country,
+      city: city
+    })
+  });
+
+  res.status(200).json({ ip, ua, country, city });
 }
