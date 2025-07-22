69d65100a88a292001c1a1be9f9a2dee44c644c9
boolang24 <dafadaxx023@gmail.com>
2025-07-22 12:00:28 +0700

    Update ip.js

--- api/ip.js
+++ api/ip.js
@@ -2,20 +2,18 @@ import crypto from "crypto";
 
 export default async function handler(req, res) {
   const ip = req.headers["x-forwarded-for"]?.split(',')[0] || req.socket.remoteAddress;
-
   const hash = crypto.createHash("sha256").update(ip).digest("hex").substring(0, 16);
 
   const sheetURL = "https://script.google.com/macros/s/AKfycbz52lT0lKEUDIHjhc9xQ_RIOfqO9EchaF1Jk-WCcoNyxdHiJyW5jhRLgT6e8yR6_Ok/exec";
 
   try {
-    // Cek apakah hash sudah ada di Google Sheet
     const checkURL = `${sheetURL}?check=${hash}`;
     const checkRes = await fetch(checkURL);
     const checkText = await checkRes.text();
 
     const clicked = checkText.includes("FOUND");
 
-    // Log IP ke sheet (untuk mencatat baru)
+    // Simpan IP + Hash ke spreadsheet
     await fetch(`${sheetURL}?ip=${ip}&hash=${hash}`);
 
     res.status(200).json({
